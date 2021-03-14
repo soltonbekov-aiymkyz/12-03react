67c381a0052577fa4c592a26476991caffc90f7a
Your Name <you@example.com>
2021-03-14 05:29:40 -0700

    delete person

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,17 +1,5 @@
 import React from 'React'
-/*import classes from "./Person.module.css";
-const Person = ({ name, role, wpm, commits }) => {
-    return (
-        <div className={classes.Person}>
-            <div className={classes.name}>{name}</div>
-            <div className={classes.role}>{role}</div>
-            <div className={classes.wpm}>{wpm}WPM</div>
-            <div className={classes.commits}>{commits} commits</div>
-        </div>
-    );
-}
-export default Person;
-*/
+
 import classes from "./Person.module.css";
 const Person = ({name, photo, role, wpm, commits}) => {
         return (
