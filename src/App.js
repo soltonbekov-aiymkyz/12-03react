54119ad0274e511f396e5ec59b3c4a14c0a72715
Your Name <you@example.com>
2021-03-15 00:35:32 -0700

    added photo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     { name: "Tynchtyk", role: "Teacher", wpm: 102, commits: 0 },
     { name: "Danila",       role: "Student", wpm: -5, commits: 1244 },
     { name: "Madina", photo: "https://avatars.githubusercontent.com/u/75364399?s=460&u=bc77810078d496e3dfe66f27906b01f5db7f208b&v=4 " ,    role: "Student", wpm: 30, commits: 5000 },
-    { name: "Tatyna", role: "Student", wpm: 27, commits: 572541 },
+    { name: "Tatyna",photo:"https://avatars.githubusercontent.com/u/75364411?s=460&u=b024420280528948f320ad191779a670439e54ec&v=4", role: "Student", wpm: 27, commits: 572541 },
     { name: "Emirlan", photo:"https://avatars.githubusercontent.com/u/75364762?s=460&u=1945ae5a1c13d5c73dd12e500b24e1965c06decc&v=4", role: "Student", wpm: 20, commits: 56566 },
     { name: "Baitemir",photo:"https://avatars.githubusercontent.com/u/65547734?s=460&u=7fc2c86db4a7a0feaef6f973739812aa62546604&v=4", role: "Student", wpm: 35, commits: 556 },
     { name: "Yntymak", photo:"https://avatars.githubusercontent.com/u/75364447?s=460&u=e5cab283f880cac68f5e9cec3b09e4559c5f5b8f&v=4", role: "Student", wpm: 40, commits: 55 },
