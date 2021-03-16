a7539803781a81852e0d1ecd424f09ff50c04650
Your Name <you@example.com>
2021-03-16 07:17:13 -0700

    person list

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -8,8 +8,20 @@ const PersonList = ({ persons, filterStudents, filterWPM, filterCommits }) => {
  if (filterStudents) {
    students = persons.filter(person => person.role == "Student");
  }
-
- 
+ // Korotkiy
+ // const students = persons.filter(person => !filterStudents || person.role == "Student");
+ let wpm = students;
+ if (filterWPM) {
+   wpm = students.filter(person => person.wpm > 34);
+ }
+ let commits = wpm;
+ if (filterCommits) {
+   commits = students.filter(person => person.commits > 999);
+ }
+ return (
+   <div className={classes.PersonList}>
+     {commits.map(person => <Person {...person} />)}
+   </div>
  );
 }
 export default PersonList;
