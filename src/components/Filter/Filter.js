fe3f441cee08ca670d8e93fee750def08af006d3
Your Name <you@example.com>
2021-03-16 07:11:02 -0700

    filter js fill

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,1 +1,12 @@
 import classes from "./Filter.module.css";
+const Filter = ({ state, change, children }) => {
+    return ( 
+      <div className={classes.Filter}>
+        <label>
+          <input type="checkbox" checked={state} onChange={() => change(!state)} /> {children}
+        </label>
+      </div>
+     );
+  }
+   
+  export default Filter;
