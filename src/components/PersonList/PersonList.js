b6aff80b5d4d4a1aa37a65eb0c254c029c0c81ad
Your Name <you@example.com>
2021-03-16 07:15:36 -0700

    person list file changed

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -1,12 +1,6 @@
 
 import Person from "../Person/Person";
 import classes from "./PersonList.module.css";
-const PersonList = ({ persons }) => {
-    return (
-        <div className={classes.PersonList}>
-            {persons.map(person => <Person {...person} />)}
-        </div>
-    );
-}
-export default PersonList;
 
+const PersonList = ({ persons, filterStudents, filterWPM, filterCommits }) => {
+  
