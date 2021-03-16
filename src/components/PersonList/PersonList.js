49d33239add472c13f8ce70ca86e3a9740b236e4
Your Name <you@example.com>
2021-03-16 07:16:48 -0700

    person list file

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -3,6 +3,13 @@ import Person from "../Person/Person";
 import classes from "./PersonList.module.css";
 
 const PersonList = ({ persons, filterStudents, filterWPM, filterCommits }) => {
- 
+ // Dlinnyi
+ let students = persons;
+ if (filterStudents) {
+   students = persons.filter(person => person.role == "Student");
+ }
 
+ 
+ );
+}
 export default PersonList;
